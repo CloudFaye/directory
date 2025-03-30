fd1280b48b9a2639a9181cf6a122894e5b58d609
Faye seun <samuelfaye96@gmail.com>
2025-03-30 20:02:25 +0100

    Remove legacy _redirects file and update Svelte configuration to use Vercel adapter; add new dependencies for Vercel support.

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,10 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      edge: false,
-      split: false
-    }),
+    adapter: adapter(),
     prerender: {
       entries: [] 
     }
