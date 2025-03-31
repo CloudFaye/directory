1ea749fe23bd9e540fda68ddcaec3cdd1b4a39b0
Faye seun <samuelfaye96@gmail.com>
2025-03-31 17:26:49 +0100

    Added vercel speed insights to root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,15 @@
 <script>
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import '../app.css';
 	let { children } = $props();
+	injectSpeedInsights();
 </script>
 
 <main class="p-8">
 	{@render children()}
+	
 </main>
+
+
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,7 +29,6 @@
 </script>
 
 <div class="container mx-auto p-4">
-
   
   {#if loading}
     <div class="flex justify-center py-4">
